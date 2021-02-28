f5d5203d8abd32e5d5fe1b633611055c580c2e51
Marius Sørensen <jobb@s0rensen.no>
2021-02-28 14:38:51 +0100

    Moved folder

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import Button from "./components/Button";
+import Button from "../components/Button";
 
 const Home = () => {
     const history  = useHistory();
