c4d26d1a518b7eeebdfb1fb1efdb1e8b52ddda87
Marius Sørensen <jobb@s0rensen.no>
2021-03-07 18:20:21 +0100

    Added Play Again

--- src/views/client/GameStateComponents/GameEnded.tsx
+++ src/views/client/GameStateComponents/GameEnded.tsx
@@ -1,6 +1,12 @@
+import Button from "../../../components/Button/Button";
 import Game from "../../../models/Game";
 
-const GameEnded = ({game} : {game: Game}) => {
+interface IGameEnded {
+    game: Game
+    resetGame: () => void
+}
+
+const GameEnded = ({game, resetGame}: IGameEnded) => {  
     const evilWin = game.pointsEvil > game.pointsInnocent
 
     return(
@@ -8,6 +14,11 @@ const GameEnded = ({game} : {game: Game}) => {
             <h1>The game has ended</h1>
             <h2>{evilWin ? 'Evil' : 'Good'} won!</h2>
             <h3>{game.pointsInnocent} - {game.pointsEvil}</h3>
+            <Button
+                onClick = {resetGame}
+            >
+                Play Again
+            </Button>
         </>
     )
 }
