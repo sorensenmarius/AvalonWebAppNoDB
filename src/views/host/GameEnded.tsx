3c1556038f209cd5b43a8762b84da9fbdb16e7ef
Marius Sørensen <jobb@s0rensen.no>
2021-03-16 18:13:47 +0100

    Add Avatars to Game Ended screen

--- src/views/host/GameEnded.tsx
+++ src/views/host/GameEnded.tsx
@@ -1,6 +1,9 @@
-import { useEffect, useState } from "react";
+import AvatarComponent from "avataaars";
+import React, { useEffect, useState } from "react";
 import Button from "../../components/Button/Button";
 import IGameProp from "../../models/IGameProp";
+import Player from "../../models/Player";
+import { AvatarDefaultSettings } from "../client/Helpers/Avatars/IAvatar";
 
 import './GameEnded.css'
 
@@ -10,24 +13,21 @@ interface IGameEndedProps extends IGameProp{
 
 const GameEnded = ({game, resetGame}: IGameEndedProps) => {
     const [goodWon, setGoodWon] = useState<boolean>()
-    const [winnerNames, setWinnerNames] = useState<string>()
+    const [winnerTeam, setWinnerTeam] = useState<Player[]>([])
 
     useEffect(() => {
         const tmpGoodWon = game.pointsInnocent > game.pointsEvil
 
         const goodTeam = game.players.filter(p => !p.isEvil)
         const evilTeam = game.players.filter(p => p.isEvil)
-        const winnerTeam = tmpGoodWon ? goodTeam : evilTeam
+        setWinnerTeam(tmpGoodWon ? goodTeam : evilTeam)
 
         setGoodWon(tmpGoodWon)
-        setWinnerNames(winnerTeam.map(p => p.name).join(', '))
     }, [game.players, game.pointsEvil, game.pointsInnocent])
 
     return(
         <div className={`game-ended-background-host ${goodWon ? 'good' : 'evil'}`}>
             <h1>Congratulations to the {goodWon ? 'good' : 'evil'} team!</h1>
-            <h2>{winnerNames}</h2>
-
             {game.pointsInnocent >= 0 ?
                 <>
                     <h3>Good had {game.pointsInnocent} point{game.pointsInnocent !== 1 ? 's' : ''}</h3>
@@ -36,6 +36,18 @@ const GameEnded = ({game, resetGame}: IGameEndedProps) => {
             : 
                 <h3>Evil won by assassinating Merlin!</h3>
             }
+            <div className="game-ended-winners-container">
+                {winnerTeam.map(p => (
+                    <div className="game-ended-player-container" key={p.id}>
+                        <AvatarComponent
+                            style={{height: '50%'}}
+                            {...AvatarDefaultSettings}
+                            {...p.avatar}
+                        />
+                        <h2>{p.name}</h2>
+                    </div>
+                ))}
+            </div>
             <Button
                 onClick={resetGame}
             >
