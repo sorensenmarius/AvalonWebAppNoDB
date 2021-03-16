48adfbe1f9c6efbac4ea20b463f38716f9982c34
Marius Sørensen <jobb@s0rensen.no>
2021-03-16 18:27:18 +0100

    Styled Host Assassin Screen

--- src/views/host/AssassinTurnHost.tsx
+++ src/views/host/AssassinTurnHost.tsx
@@ -1,17 +1,31 @@
+import AvatarComponent from "avataaars";
 import { useEffect, useState } from "react";
 import Role from "../../models/enums/Roles";
 import IGameProp from "../../models/IGameProp";
+import Player from "../../models/Player";
+import { AvatarDefaultSettings } from "../client/Helpers/Avatars/IAvatar";
+
+import './AssassinTurnHost.css'
 
 const AssassinTurnHost = ({game}: IGameProp) => {
-    const [assassinName, setAssassinName] = useState<string>('')
+    const [assassin, setAssassin] = useState<Player | undefined>()
 
     useEffect(() => {
-        const tmp = game.players.find(p => p.roleId === Role.Assassin)?.name
-        setAssassinName(tmp ? tmp : '')
+        const tmp = game.players.find(p => p.roleId === Role.Assassin)
+        setAssassin(tmp)
     }, [game.players])
 
     return(
-        <h1>{assassinName} is going to try to assassinate Merlin!!</h1>
+        <>
+            <div className='assassin-host-background' />
+            <div className='assassin-host-container'>
+                <h1 className='assassin-host-text'>{assassin?.name} is going to try to assassinate Merlin!!</h1>
+                <AvatarComponent
+                    {...AvatarDefaultSettings}
+                    {...assassin?.avatar}
+                />
+            </div>
+        </>
     )    
 }
 
